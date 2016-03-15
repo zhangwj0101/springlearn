8035323a5edb0f6b31d54af48502708c17521280
zhangwj0101 <zhangwj0101@gmail.com>
2016-03-15 15:00:18 +0800

    fix

--- springlearn/src/test/java/cn/nlsde/base/BaseTest.java
+++ springlearn/src/test/java/cn/nlsde/base/BaseTest.java
@@ -1,10 +1,8 @@
 package cn.nlsde.base;
 
-import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
@@ -13,8 +11,8 @@ import org.springframework.test.context.transaction.TransactionalTestExecutionLi
  */
 //@RunWith(SpringJUnit4ClassRunner.class)
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
-                         TransactionalTestExecutionListener.class})
+        TransactionalTestExecutionListener.class})
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/ApplicationContext.xml"})
-public class BaseTest extends AbstractJUnit4SpringContextTests {
+public abstract class BaseTest extends AbstractJUnit4SpringContextTests {
 
 }
